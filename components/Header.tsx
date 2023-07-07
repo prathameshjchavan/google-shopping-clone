458869f5dfbe8c561182593f47c9bd2b6cac341d
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-07-07 11:25:43 +0530

    building form section

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 
 type Props = {};
 
@@ -18,7 +19,24 @@ const Header = (props: Props) => {
 				/>
 			</Link>
 
-			<div></div>
+			<div className="w-full md:max-w-2xl">
+				{/* FORM */}
+				<form action="">
+					<div className="flex items-center gap-2 w-full px-4">
+						<div className="flex items-center space-x-2 bg-white shadow-xl rounded-full border-0 px-6 py-4 flex-1">
+							<MagnifyingGlassIcon className="h-5 w-5 text-gray-400" />
+							<input
+								name="searchTerm"
+								type="text"
+								placeholder="Search..."
+								className="outline-none flex-1"
+							/>
+						</div>
+
+						{/* Search */}
+					</div>
+				</form>
+			</div>
 		</header>
 	);
 };
