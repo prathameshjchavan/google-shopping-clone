51461eeff253dd99166fa975e09386de9b801adc
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-07-08 18:42:49 +0530

    implemented react loading skeleton

--- app/shopping/product/[id]/not-found.tsx
+++ app/shopping/product/[id]/not-found.tsx
@@ -1,7 +1,14 @@
 type Props = {};
 
 const NotFound = (props: Props) => {
-	return <div>NotFound</div>;
+	return (
+		<div className="flex flex-col justify-center items-center space-y-5">
+			<h1 className="text-4xl font-bold">Whoops...</h1>
+			<h2 className="font-extralight animate-pulse">
+				It looks like the Product could not be found!
+			</h2>
+		</div>
+	);
 };
 
 export default NotFound;
